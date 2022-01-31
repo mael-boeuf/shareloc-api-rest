2cecafae45d493c974090bb9cbfdf12986dce0e5
mboeuf <66299300+mboeuf@users.noreply.github.com>
2022-01-31 21:46:16 +0100

    Modified User class

--- src/main/java/ensisa/boeuf/jacquey/tekin/shareloc/model/User.java
+++ src/main/java/ensisa/boeuf/jacquey/tekin/shareloc/model/User.java
@@ -1,4 +1,59 @@
 package ensisa.boeuf.jacquey.tekin.shareloc.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class User {
+
+    @Id
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(String _email, String _password, String _firstName, String _lastName) {
+        this.email = _email;
+        this.password = _password;
+        this.firstName = _firstName;
+        this.lastName = _lastName;
+    }
+
+    public User() {
+        super();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
