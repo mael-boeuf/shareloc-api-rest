622bdceac87b26024037a1284a9be4b79d2d286e
mboeuf <66299300+mboeuf@users.noreply.github.com>
2022-01-31 21:50:05 +0100

    Modified Service class

--- src/main/java/ensisa/boeuf/jacquey/tekin/shareloc/model/Service.java
+++ src/main/java/ensisa/boeuf/jacquey/tekin/shareloc/model/Service.java
@@ -1,4 +1,49 @@
 package ensisa.boeuf.jacquey.tekin.shareloc.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Service {
+
+    @Id
+    private String title;
+    private String description;
+    private int cost;
+
+    public Service(String title, String description, int cost) {
+        this.title = title;
+        this.description = description;
+        this.cost = cost;
+    }
+
+    public Service() {
+        super();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 }
