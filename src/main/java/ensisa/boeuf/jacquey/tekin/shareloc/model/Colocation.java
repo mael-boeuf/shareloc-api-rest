23d5996778e0093afcda3c5d089d9806022c814d
mboeuf <66299300+mboeuf@users.noreply.github.com>
2022-01-31 21:47:58 +0100

    Modified Colocation class

--- src/main/java/ensisa/boeuf/jacquey/tekin/shareloc/model/Colocation.java
+++ src/main/java/ensisa/boeuf/jacquey/tekin/shareloc/model/Colocation.java
@@ -1,4 +1,29 @@
 package ensisa.boeuf.jacquey.tekin.shareloc.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Colocation {
+
+    @Id
+    private String name;
+
+    public Colocation(String name) {
+        this.name = name;
+    }
+
+    public Colocation() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
